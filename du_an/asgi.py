2d0c66ab641fe472227ea399861420b95e00871f
linh-mac <phanlinher@gmail.com>
2024-05-14 22:37:58 +0700

    init app

---
+++ du_an/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for du_an project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "du_an.settings")
+
+application = get_asgi_application()
---
+++ qlhd/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QlhdConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "qlhd"
